b6a3090880dc89df1e590f5672f83e4c83ddf52c
Alister Berry <ganchy>
2026-08-13 02:12:25 -0400

    fish configuration

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,21 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+set -gx NNN_COLORS '4561'
+set -gx NNN_FCOLORS '050605020706060801010203'
+
+if test -f ~/.cache/wal/colors.fish
+    source ~/.cache/wal/colors.fish
+end
+
+function nnn
+    command nnn $argv
+
+    # Re-apply the latest pywal colors after nnn exits
+    if test -f ~/.cache/wal/colors.fish
+        source ~/.cache/wal/colors.fish
+    end
+
+    if test -f ~/.cache/wal/colors-kitty.conf
+        kitty @ set-colors --all ~/.cache/wal/colors-kitty.conf 2>/dev/null
+    end
+end
